58cb1b160749b90c311ebf10ea494547c18139d4
Patrik Svensson <ps@molamil.com>
2015-10-15 13:43:42 +0200

    OnBoarding: Video shown without black flicker. Video aspect is wrong. Video is NOT looping

--- Android/app/src/main/java/com/molamil/radio24syv/IntroActivity.java
+++ Android/app/src/main/java/com/molamil/radio24syv/IntroActivity.java
@@ -4,29 +4,67 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.VideoView;
+
+import com.molamil.radio24syv.view.RadioViewPager;
+
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.UpdateManager;
 
-public class IntroActivity extends Activity {
+public class IntroActivity extends FragmentActivity {
 
+    private RadioViewPager pager; // The pager widget, which handles animation and allows swiping horizontally to access side screens
+    private int currentPage = 0;
+    private int previousPage = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        Button nextButton = (Button)findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        pager = (RadioViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new IntroPagerAdapter(getSupportFragmentManager())); // The pager adapter, which provides the pages to the view pager widget
+        //pager.setPageTransformer(false, pageTransformer);
+        pager.setOverScrollMode(ViewPager.OVER_SCROLL_NEVER); // No feedback when trying to scroll but there are no next page (Android 4 blue edge tint)
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
             @Override
-            public void onClick(View v) {
-                startMainActivity();
+            public void onPageScrollStateChanged(int state)
+            {
+                if(state == ViewPager.SCROLL_STATE_IDLE)
+                {
+                    updateOnPageChange();
+                }
             }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+
+            }
+
+            @Override
+            public void onPageSelected(int position)
+            {
+                previousPage = currentPage;
+                currentPage = position;
+
+            }
+
         });
     }
 
@@ -34,7 +72,7 @@ public class IntroActivity extends Activity {
     protected void onStart() {
         super.onStart();
 
-        firstRunCheck();
+        returningUserCheck();
     }
 
     private void startMainActivity() {
@@ -43,18 +81,106 @@ public class IntroActivity extends Activity {
         finish(); // Stop this activity
     }
 
-    private void firstRunCheck()
+    private void returningUserCheck()
     {
         String name = getApplicationContext().getPackageName();
         SharedPreferences settings = getSharedPreferences(name, Context.MODE_PRIVATE);
 
         if (settings.getBoolean("returningUser", false))
         {
-            startMainActivity();
+            //Un-comment for correct flow
+            //startMainActivity();
         }
         else
         {
             settings.edit().putBoolean("returningUser", true).commit();
+        }
+    }
+
+    private void updateOnPageChange()
+    {
+        cleanupVideo(previousPage);
+
+        if(currentPage == 0)
+        {
+            //
+        }
+        else if (currentPage == pager.getAdapter().getCount()-1)
+        {
+            pager.setPagingEnabled(false);
+            startMainActivity();
+        }
+        else
+        {
+            //prepare video
+            playVideo(currentPage);
+        }
+    }
+
+    /*
+    Video
+     */
+    private void cleanupVideo(int position)
+    {
+        IntroPagerAdapter adapter = ((IntroPagerAdapter)pager.getAdapter());
+        OnboardingPageFragment fragment = (OnboardingPageFragment)adapter.getRegisteredFragment(position);
+        if(fragment != null)
+        {
+            fragment.cleanupVideo();
+        }
+    }
+
+    private void playVideo(int position)
+    {
+        IntroPagerAdapter adapter = ((IntroPagerAdapter)pager.getAdapter());
+        OnboardingPageFragment fragment = (OnboardingPageFragment)adapter.getRegisteredFragment(position);
+        if(fragment != null)
+        {
+            fragment.playVideo();
+        }
+    }
+
+    /*
+    Custom page adaptors - one for each tab on the main screen.
+    This is the best way to change contents of ViewPager on the fly.
+     */
+    private class IntroPagerAdapter extends FragmentStatePagerAdapter
+    {
+        SparseArray<OnboardingPageFragment> registeredFragments = new SparseArray<OnboardingPageFragment>();
+
+        public IntroPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            OnboardingPageFragment fragment = new OnboardingPageFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            args.putString("packageName", getPackageName());
+            fragment.setArguments(args);
+            registeredFragments.put(position, fragment);
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return 6; //intro + 4x Video + empty that loads main
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object)
+        {
+            registeredFragments.remove(position);
+            super.destroyItem(container, position, object);
+        }
+
+        public Fragment getRegisteredFragment(int position)
+        {
+            return registeredFragments.get(position);
         }
     }
 }
---
+++ Android/app/src/main/java/com/molamil/radio24syv/OnboardingPageFragment.java
@@ -0,0 +1,148 @@
+package com.molamil.radio24syv;
+
+import android.app.ActionBar;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.VideoView;
+
+import com.molamil.radio24syv.videoView.AspectFillVideoView;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * Created by patriksvensson on 09/10/15.
+ */
+public class OnboardingPageFragment extends Fragment
+{
+    private int position = -1;
+    private int mVideoWidth = 720;
+    private int mVideoHeight = 720;
+    private String videoPath;
+    private VideoView videoView;
+
+    //private TextureView textureView;
+    //private MediaPlayer mMediaPlayer;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt("position");
+        String packageName = getArguments().getString("packageName");
+        int videoIndex = position - 1;
+
+        //NB! Update vW & vH when updating videos
+        switch (videoIndex)
+        {
+            case 0:
+                videoPath = "android.resource://" + packageName + "/"+R.raw.live;
+                mVideoWidth = 718;
+                break;
+            case 1:
+                videoPath = "android.resource://" + packageName + "/"+R.raw.programmer;
+                mVideoWidth = 716;
+                break;
+            case 2:
+                videoPath = "android.resource://" + packageName + "/"+R.raw.news;
+                mVideoWidth = 720;
+                break;
+            case 3:
+                videoPath = "android.resource://" + packageName + "/"+R.raw.offline;
+                mVideoWidth = 718;
+                break;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        int resourceID = R.layout.fragment_onboarding_video_page;
+        if(position == 0)
+        {
+            resourceID = R.layout.fragment_onboarding_welcome_page;
+        }
+        else if(position == 5)
+        {
+            resourceID = R.layout.fragment_onboarding_main_page;
+        }
+
+        View v = inflater.inflate(resourceID, container, false);
+        videoView = (VideoView)v.findViewById(R.id.video);
+
+        return v;
+    }
+
+    // Video control
+    public void playVideo()
+    {
+        Log.i("PS", "playVideo (called from pager)");
+        //Using SurfaceView and MediaPlayer
+        //surfaceView.setVisibility(View.VISIBLE);
+
+        //Using VideoView
+        if(videoView != null && videoPath != null)
+        {
+
+            //ViewGroup.LayoutParams params = videoView.getLayoutParams();
+            //Log.i("PS", "videoView.getWidth(): "+videoView.getWidth());
+            //Log.i("PS", "videoView.getHeight(): "+videoView.getHeight());
+            //params.width = videoView.getWidth()*100;
+            //params.height = videoView.getHeight();
+
+
+            //videoView.setAlpha(0);
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+
+                    videoView.start();
+                    //HACK: Show videoview AFTER it has started playing
+                    new Handler().post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(videoView.getCurrentPosition() != 0){
+                                Log.i("PS", "video prepared, show it");
+                                //View placeholder = findViewById(R.id.placeholder);
+                                //placeholder.setVisibility(View.GONE);
+                                videoView.setAlpha(1);
+                            } else {
+                                new Handler().postDelayed(this, 50);
+                            }
+                        }
+                    });
+                }
+            });
+
+
+            //videoView.start();
+            //videoView.setVideoSize(mVideoWidth, mVideoWidth);
+            videoView.setVideoPath(videoPath);
+        }
+    }
+
+    public void cleanupVideo()
+    {
+        if(videoView != null)
+        {
+            //TODO: Stop and unload video
+            //videoView.setVisibility(View.GONE);
+            videoView.setAlpha(0);
+        }
+    }
+
+}
