9388f09050119f5381f869f6f913b4f317983891
Jens Juul Jacobsen <jjj@appropos.dk>
2016-05-10 11:45:29 +0200

    Fixed crash "IllegalArgumentException: Service Intent must be explicit" when using notification center widget to play stream on Android 5+

--- Android/app/src/main/java/com/molamil/radio24syv/receiver/RemoteControlReceiver.java
+++ Android/app/src/main/java/com/molamil/radio24syv/receiver/RemoteControlReceiver.java
@@ -54,27 +54,27 @@ public class RemoteControlReceiver extends BroadcastReceiver {
                 case KeyEvent.KEYCODE_HEADSETHOOK:
                 case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
                     Log.d("PS", "RemoteControlReceiver.Play_Pause");
-                    context.startService(new Intent(RadioPlayerService.ACTION_TOGGLE_PLAYBACK));
+                    startService(context, RadioPlayerService.ACTION_TOGGLE_PLAYBACK);
                     break;
                 case KeyEvent.KEYCODE_MEDIA_PLAY:
                     Log.d("PS", "RemoteControlReceiver.Play");
-                    context.startService(new Intent(RadioPlayerService.ACTION_PLAY));
+                    startService(context, RadioPlayerService.ACTION_PLAY);
                     break;
                 case KeyEvent.KEYCODE_MEDIA_PAUSE:
                     Log.d("PS", "RemoteControlReceiver.Pause");
-                    context.startService(new Intent(RadioPlayerService.ACTION_PAUSE));
+                    startService(context, RadioPlayerService.ACTION_PAUSE);
                     break;
                 case KeyEvent.KEYCODE_MEDIA_STOP:
                     Log.d("PS", "RemoteControlReceiver.Stop");
-                    context.startService(new Intent(RadioPlayerService.ACTION_STOP));
+                    startService(context, RadioPlayerService.ACTION_STOP);
                     break;
                 case KeyEvent.KEYCODE_MEDIA_NEXT:
                     Log.d("PS", "RemoteControlReceiver.NExt");
-                    context.startService(new Intent(RadioPlayerService.ACTION_NEXT));
+                    startService(context, RadioPlayerService.ACTION_NEXT);
                     break;
                 case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
                     Log.d("PS", "RemoteControlReceiver.Prev");
-                    context.startService(new Intent(RadioPlayerService.ACTION_PREVIOUS));
+                    startService(context, RadioPlayerService.ACTION_PREVIOUS);
                     break;
             }
         }
@@ -83,44 +83,40 @@ public class RemoteControlReceiver extends BroadcastReceiver {
         else if(intent.getAction().equals(RadioPlayerService.ACTION_TOGGLE_PLAYBACK))
         {
             Log.i("PS", "Widget action: ACTION_TOGGLE_PLAYBACK");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_TOGGLE_PLAYBACK);
-            context.startService(serviceIntent);
+            startService(context, RadioPlayerService.ACTION_TOGGLE_PLAYBACK);
         }
         else if(intent.getAction().equals(RadioPlayerService.ACTION_NEXT))
         {
             Log.i("PS", "Widget action: ACTION_NEXT");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_NEXT);
-            context.startService(serviceIntent);
-        } else if(intent.getAction().equals(RadioPlayerService.ACTION_PREVIOUS))
+            startService(context, RadioPlayerService.ACTION_NEXT);
+        }
+        else if(intent.getAction().equals(RadioPlayerService.ACTION_PREVIOUS))
         {
             Log.i("PS", "Widget action: ACTION_PREVIOUS");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_PREVIOUS);
-            context.startService(serviceIntent);
+            startService(context, RadioPlayerService.ACTION_PREVIOUS);
         }
         else if(intent.getAction().equals(RadioPlayerService.ACTION_PLAY))
         {
             Log.i("PS", "Widget action: ACTION_PLAY");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_PLAY);
-            context.startService(serviceIntent);
+            startService(context, RadioPlayerService.ACTION_PLAY);
         }
         else if(intent.getAction().equals(RadioPlayerService.ACTION_STOP))
         {
             Log.i("PS", "Widget action: ACTION_STOP");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_STOP);
-            context.startService(serviceIntent);
+            startService(context, RadioPlayerService.ACTION_STOP);
         }
         else if(intent.getAction().equals(RadioPlayerService.ACTION_PAUSE))
         {
             Log.i("PS", "Widget action: ACTION_PAUSE");
-            Intent serviceIntent = new Intent(context,RadioPlayerService.class);
-            serviceIntent.setAction(RadioPlayerService.ACTION_PAUSE);
-            context.startService(serviceIntent);
+            startService(context, RadioPlayerService.ACTION_PAUSE);
         }
 
+    }
+
+    // Explicit service intent (the class is set in the intent) is required for Android 5+ or else app crashes with error "IllegalArgumentException: Service Intent must be explicit"
+    private static void startService(Context context, String action) {
+        Intent serviceIntent = new Intent(context, RadioPlayerService.class);
+        serviceIntent.setAction(action);
+        context.startService(serviceIntent);
     }
 }
